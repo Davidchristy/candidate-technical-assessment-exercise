7b254034eff52487dc9098a2a1f8f72cb8728eb3
David Christy <DavidChristy520@gmail.com>
2021-10-07 11:15:53 -0700

    Adding Try catch to unit test for confirm DriverManager closes

--- src/test/java/com/gaggle/assessment_1/SQLiteServiceTests.java
+++ src/test/java/com/gaggle/assessment_1/SQLiteServiceTests.java
@@ -30,107 +30,132 @@ public class SQLiteServiceTests {
 
 
     @Test
-	void testAddContactValid() throws SQLException, RepositoryException {
+	void testAddContactValid() {
         Connection conn = mock(Connection.class);
         Statement stmt = mock(Statement.class);
         MockedStatic<DriverManager> DriverManagerMock =  mockStatic(DriverManager.class);
-        when(conn.createStatement()).thenReturn(stmt);
-        when(DriverManager.getConnection(anyString())).thenReturn(conn);
-        SQLiteDatabaseService db = new SQLiteDatabaseService();
-        verify(stmt,times(1)).execute(anyString());
-        db.addContact("test");
-        verify(stmt,times(2)).execute(anyString());
-        DriverManagerMock.close();
+        try{
+            when(conn.createStatement()).thenReturn(stmt);
+            when(DriverManager.getConnection(anyString())).thenReturn(conn);
+            SQLiteDatabaseService db = new SQLiteDatabaseService();
+            verify(stmt,times(1)).execute(anyString());
+            db.addContact("test");
+            verify(stmt,times(2)).execute(anyString());
+        } catch (SQLException | RepositoryException e){
+            e.printStackTrace();
+        } finally {
+            DriverManagerMock.close();
+        }
 	}
 
     @Test
-	void testConnectionError() throws SQLException, RepositoryException {
+	void testConnectionError() {
         Connection conn = mock(Connection.class);
         Statement stmt = mock(Statement.class);
         MockedStatic<DriverManager> DriverManagerMock =  mockStatic(DriverManager.class);
-        when(DriverManager.getConnection(anyString())).thenReturn(conn);
-        when(conn.createStatement()).thenThrow(SQLException.class);
-        assertThrows(RepositoryException.class, ()->{
-            new SQLiteDatabaseService();
-        });
-        verify(stmt,times(0)).execute(anyString());
-        DriverManagerMock.close();
+        try {
+            when(DriverManager.getConnection(anyString())).thenReturn(conn);
+            when(conn.createStatement()).thenThrow(SQLException.class);
+            assertThrows(RepositoryException.class, ()->{
+                new SQLiteDatabaseService();
+            });
+            verify(stmt,times(0)).execute(anyString());
+        } catch (SQLException e){
+            e.printStackTrace();
+        } finally {
+            DriverManagerMock.close();
+        }
 	}
 
     @Test
-    void testGetNameByIDNoEntry() throws SQLException, RepositoryException {
+    void testGetNameByIDNoEntry() {
         Connection conn = mock(Connection.class);
         Statement stmt = mock(Statement.class);
         ResultSet resultSet = mock(ResultSet.class);
         MockedStatic<DriverManager> DriverManagerMock =  mockStatic(DriverManager.class);
-        when(DriverManager.getConnection(anyString())).thenReturn(conn);
-        when(conn.createStatement()).thenReturn(stmt);
-        when(stmt.executeQuery(anyString())).thenReturn(resultSet);
-        when(resultSet.next()).thenReturn(false);
-        SQLiteDatabaseService db = new SQLiteDatabaseService();
-        assertThrows(EntryNotFoundException.class, ()->{
-            db.getNameByID(1);
-        });
-        DriverManagerMock.close();
+        try{
+            when(DriverManager.getConnection(anyString())).thenReturn(conn);
+            when(conn.createStatement()).thenReturn(stmt);
+            when(stmt.executeQuery(anyString())).thenReturn(resultSet);
+            when(resultSet.next()).thenReturn(false);
+            SQLiteDatabaseService db = new SQLiteDatabaseService();
+            assertThrows(EntryNotFoundException.class, ()->{
+                db.getNameByID(1);
+            });
+        } catch (SQLException | RepositoryException e){
+            e.printStackTrace();
+        } finally {
+            DriverManagerMock.close();
+        }
     }
 
     @Test
-    void testGetNameByIDValid() throws SQLException, RepositoryException, EntryNotFoundException {
+    void testGetNameByIDValid() {
         Connection conn = mock(Connection.class);
         Statement stmt = mock(Statement.class);
         ResultSet resultSet = mock(ResultSet.class);
         MockedStatic<DriverManager> DriverManagerMock =  mockStatic(DriverManager.class);
-        when(DriverManager.getConnection(anyString())).thenReturn(conn);
-        when(conn.createStatement()).thenReturn(stmt);
-        when(stmt.executeQuery(anyString())).thenReturn(resultSet);
-        when(resultSet.next()).thenReturn(true);
-        when(resultSet.getString("name")).thenReturn("David");
-        SQLiteDatabaseService db = new SQLiteDatabaseService();
-        String name = db.getNameByID(1);
-        assertEquals("David", name);
-        DriverManagerMock.close();
+        try {
+            when(DriverManager.getConnection(anyString())).thenReturn(conn);
+            when(conn.createStatement()).thenReturn(stmt);
+            when(stmt.executeQuery(anyString())).thenReturn(resultSet);
+            when(resultSet.next()).thenReturn(true);
+            when(resultSet.getString("name")).thenReturn("David");
+            SQLiteDatabaseService db = new SQLiteDatabaseService();
+            String name = db.getNameByID(1);
+            assertEquals("David", name);
+        } catch (SQLException | RepositoryException | EntryNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            DriverManagerMock.close();
+        }
     }
 
     @Test
-    void testSearchBasedOnNameError() throws SQLException, RepositoryException {
+    void testSearchBasedOnNameError() {
         Connection conn = mock(Connection.class);
         Statement stmt = mock(Statement.class);
         MockedStatic<DriverManager> DriverManagerMock =  mockStatic(DriverManager.class);
-        when(DriverManager.getConnection(anyString())).thenReturn(conn);
-        when(conn.createStatement()).thenReturn(stmt);
-        when(stmt.executeQuery(anyString())).thenThrow(SQLException.class);
-        SQLiteDatabaseService db = new SQLiteDatabaseService();
-        assertThrows(RepositoryException.class, ()->{
-            db.searchBasedOnName("Test",1);
-        });
-        DriverManagerMock.close();
+        try {
+            when(DriverManager.getConnection(anyString())).thenReturn(conn);
+            when(conn.createStatement()).thenReturn(stmt);
+            when(stmt.executeQuery(anyString())).thenThrow(SQLException.class);
+            SQLiteDatabaseService db = new SQLiteDatabaseService();
+            assertThrows(RepositoryException.class, ()->{
+                db.searchBasedOnName("Test",1);
+            });
+        } catch (SQLException | RepositoryException e) {
+            e.printStackTrace();            
+        } finally {
+            DriverManagerMock.close();
+        }
     }
 
     @Test
-    void testSearchBasedOnName() throws SQLException, RepositoryException, EntryNotFoundException {
+    void testSearchBasedOnName() {
         Connection conn = mock(Connection.class);
         Statement stmt = mock(Statement.class);
         ResultSet resultSet = mock(ResultSet.class);
         MockedStatic<DriverManager> DriverManagerMock =  mockStatic(DriverManager.class);
-        when(DriverManager.getConnection(anyString())).thenReturn(conn);
-        when(conn.createStatement()).thenReturn(stmt);
-        when(stmt.executeQuery(anyString())).thenReturn(resultSet);
-        when(resultSet.next()).thenReturn(true, true, true, false);
-        when(resultSet.getString("name")).thenReturn("David", "Anderson", "Alice");
-        SQLiteDatabaseService db = new SQLiteDatabaseService();
-        ArrayList<String> names = db.searchBasedOnName("a", -1);
-        assertEquals(3, names.size());
-        // Note, names are not alphabetical here because that's done in SQLite
-        assertEquals("David", names.get(0));
-        assertEquals("Anderson", names.get(1));
-        assertEquals("Alice", names.get(2));
-        DriverManagerMock.close();
+        try{
+            when(DriverManager.getConnection(anyString())).thenReturn(conn);
+            when(conn.createStatement()).thenReturn(stmt);
+            when(stmt.executeQuery(anyString())).thenReturn(resultSet);
+            when(resultSet.next()).thenReturn(true, true, true, false);
+            when(resultSet.getString("name")).thenReturn("David", "Anderson", "Alice");
+            SQLiteDatabaseService db = new SQLiteDatabaseService();
+            ArrayList<String> names = db.searchBasedOnName("a", -1);
+            assertEquals(3, names.size());
+            // Note, names are not alphabetical here because that's done in SQLite
+            assertEquals("David", names.get(0));
+            assertEquals("Anderson", names.get(1));
+            assertEquals("Alice", names.get(2));
+        } catch (SQLException | RepositoryException e) {
+            e.printStackTrace();
+        } finally {
+            DriverManagerMock.close();
+        }
     }
-
-
-
-
-
 
 
 }
